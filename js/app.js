17e767e5b6cae5e92c4746678b6126870492cd4d
eugene.bober <eugene.bober@anventa.com>
2015-01-02 08:43:28 +0300

    remove old files

--- js/app.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by BobrD on 26.12.2014.
- */
-
-var app = angular.module('app', ['ngRoute']);
--- js/controllers/FeedController.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by BobrD on 26.12.2014.
- */
-
-app.controller('FeedController', ['$scope', function($scope) {
-
-    $scope.test = 'test';
-}]);
