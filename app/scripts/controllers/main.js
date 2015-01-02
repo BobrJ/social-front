adcce203528afae333d77003834fade259326a76
eugene.bober <eugene.bober@anventa.com>
2015-01-02 08:39:51 +0300

    config bower and grunt

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name socialFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the socialFrontApp
+ */
+angular.module('socialFrontApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
